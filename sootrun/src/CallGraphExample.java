2f0aaf41949759eb3f29a03250542ed51ea60e36
Harshita Srinivas <60603854+harshita19244@users.noreply.github.com>
2021-11-06 20:18:27 +0530

    Update CallGraphExample.java

--- sootrun/src/CallGraphExample.java
+++ sootrun/src/CallGraphExample.java
@@ -1,14 +1,31 @@
+import fj.P;
 import soot.*;
 import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.jimple.toolkits.callgraph.Targets;
 import soot.options.Options;
 import java.util.*;
 
+class Pair{
+    private String method_name;
+    private String declaring_class;
+
+    public Pair(String method_name,String declaring_class){
+        this.method_name = method_name;
+        this.declaring_class = declaring_class;
+    }
+
+    public String getDeclaring_class() {
+        return declaring_class;
+    }
+
+    public String getMethod_name() {
+        return method_name;
+    }
+}
 public class CallGraphExample
 {
     static HashMap<String,ArrayList<String>> linkmap = new HashMap<>();
-    static HashMap<String,Type> return_types = new HashMap<>();
-    static ArrayList<SootMethod> targetmethods = new ArrayList<>();
+    static HashMap<SootMethod,Pair> targetmethods = new HashMap<>();
 
 
     public static void main(String[] args) {
@@ -25,67 +42,76 @@ public class CallGraphExample
         System.out.println("Classpath: " + Scene.v().getSootClassPath());
         Scene.v().extendSootClassPath(classesDir);
         Scene.v().extendSootClassPath(newdir);
-
-        System.out.println("Classpath: " + Scene.v().getSootClassPath());
-        SootClass c = Scene.v().forceResolve("com.google.common.math.PairedStats", SootClass.BODIES);
+        SootClass c = Scene.v().forceResolve("com.google.common.util.concurrent.AbstractService", SootClass.BODIES);
         c.setApplicationClass();
         Scene.v().loadNecessaryClasses();
-        SootMethod src = c.getMethodByName("populationCovariance");
+        SootMethod src = c.getMethodByName("stopAsync");
         Scene.v().getEntryPoints().add(src);
         List entryPoints = new ArrayList();
         entryPoints.add(src);
         Scene.v().setEntryPoints(entryPoints);
         PackManager.v().getPack("cg").apply();
         CallGraph cg = Scene.v().getCallGraph();
-        recurse(cg,src,2,0);
+        recurse(cg,src,3,0);
+        System.out.println("Linkage graph given source method");
         for (Map.Entry<String,ArrayList<String>> pair : linkmap.entrySet()) {
+            System.out.println(pair.getKey() + " -> ");
             for (String m: pair.getValue()){
-                System.out.println(pair.getKey() + " -> " + m);
+                System.out.print(m + ", ");
             }
         }
-        for (Map.Entry<String,Type> pair: return_types.entrySet()){
-            System.out.println(pair.getKey() + " -> " + pair.getValue());
+        System.out.println();
+        System.out.println();
+        System.out.println("Target methods with dependency");
+        for (Map.Entry<SootMethod,Pair> s: targetmethods.entrySet()){
+            System.out.println("Method name: " + s.getValue().getMethod_name().replace("java.lang.",""));
+            System.out.println("Declaring class: "+ s.getValue().getDeclaring_class());
         }
+
+        System.out.println();
         args = argsList.toArray(new String[0]);
         soot.Main.main(args);
      }
 
     public static void recurse(CallGraph cg, SootMethod src, int k,int temp){
-        Iterator<MethodOrMethodContext> targets = new Targets(cg.edgesOutOf(src));
+        Iterator targets = new Targets(cg.edgesOutOf(src));
+        List<String> method_names = new ArrayList<>();
         while (targets.hasNext() && temp<=k) {
             SootMethod tgt = (SootMethod)targets.next();
+            //System.out.println(tgt.getName());
             //System.out.println(src.getDeclaration() + " may call " + tgt.getDeclaration());
-            if(!targetmethods.contains(tgt.getName())) {
-                targetmethods.add(tgt);
-            }
             if(!linkmap.containsKey(src.getDeclaration())){
                 linkmap.put(src.getDeclaration(),new ArrayList<>());
             }
-            linkmap.get(src.getDeclaration()).add(tgt.getDeclaration());
-            return_types.put(tgt.getDeclaration(), tgt.getReturnType());
-
-            temp++;
+            if(tgt.getDeclaringClass().getName().contains("com.google")) {
+                targetmethods.put(tgt, new Pair(tgt.getDeclaration(),tgt.getDeclaringClass().getName()));
+            }
+            if(!method_names.contains(tgt.getName())){
+                method_names.add(tgt.getName());
+                linkmap.get(src.getDeclaration()).add(tgt.getDeclaration());
+                temp++;
+            }
         }
     }
 
     public static void recurseback(CallGraph cg, SootMethod src, int k,int temp){
-
-        if(temp==k){
-            return;
-        }
-
-        Iterator<MethodOrMethodContext> targets = new Targets(cg.edgesInto(src));
-
-        while (targets.hasNext()) {
+        Iterator targets = new Targets(cg.edgesInto(src));
+        List<String> method_names = new ArrayList<>();
+        while (targets.hasNext() && temp<=k) {
             SootMethod tgt = (SootMethod)targets.next();
-            System.out.println(tgt + " may be called by" + src);
-//            if(!targetmethods.contains(tgt.getName())){
-//                targetmethods.add(tgt.getName());
-//            }
-
-            temp++;
-            recurseback(cg,tgt,k,temp);
+            //System.out.println(tgt.getName());
+            //System.out.println(src.getDeclaration() + " may call " + tgt.getDeclaringClass() + " " + tgt.getReturnType() );
+            if(!linkmap.containsKey(src.getDeclaration())){
+                linkmap.put(src.getDeclaration(),new ArrayList<>());
+            }
+            if(tgt.getDeclaringClass().getName().contains("com.google")) {
+                targetmethods.put(tgt, new Pair(tgt.getDeclaration(),tgt.getDeclaringClass().getName()));
+            }
+            if(!method_names.contains(tgt.getName())){
+                method_names.add(tgt.getName());
+                linkmap.get(src.getDeclaration()).add(tgt.getDeclaration());
+                temp++;
+            }
         }
-
     }
 }
